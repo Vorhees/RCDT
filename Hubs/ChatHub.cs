63972e52af1ef69455e5771f9b16bf4eb9521562
isaiahfisher <isaiahfisher90@gmail.com>
2019-04-08 12:29:16 -0400

    added comments containing code to save once the database is configured for a chatlog.

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -12,6 +12,33 @@ namespace RCDT.Hubs
             //username = user.UserName;
 
             await Clients.All.SendAsync("ReceiveMessage", username,  message);
+            
+            //Save Chat in Database, need to figure out how our db is set up
+            //DB.SaveChat(username, message);
+
         }
+    //    public override System.Threading.Tasks.Task OnConnected()
+    //     {
+    //           // Get UserID. Assumed the user is logged before connecting to chat and userid is saved in session.
+    //           string UserID = (string)HttpContext.Current.Session["userid"];
+
+    //           // Get ChatHistory and call the client function. See below
+    //           this.GetHistory(UserID); 
+
+    //           // Get ConnID
+    //           string ConnID =  Context.ConnectionId; 
+
+    //           // Save them in DB. You got to create a DB class to handle this. (Optional)
+    //           //DB.UpdateConnID(UserID, ConnID); 
+    //     }
+
+    //     private void GetHistory(UserID)
+    //     {
+    //           // Get Chat History from DB. You got to create a DB class to handle this.
+    //           string History = DB.GetChatHistory(UserID); 
+
+    //           // Send Chat History to Client. You got to create chatHistory handler in Client side.
+    //           Clients.Caller.chatHistory(History );           
+    //     }
     }
 }
