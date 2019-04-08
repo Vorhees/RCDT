82bcc58a40ff0ac41261434b720f493e9069e927
isaiahfisher <isaiahfisher90@gmail.com>
2019-04-08 15:46:43 -0400

    a new migration for the chatLog database table

---
+++ Migrations/20190408194306_dateTime.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RCDT.Migrations
+{
+    public partial class dateTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateTime",
+                table: "ChatLog",
+                newName: "dateTime");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "dateTime",
+                table: "ChatLog",
+                newName: "DateTime");
+        }
+    }
+}
