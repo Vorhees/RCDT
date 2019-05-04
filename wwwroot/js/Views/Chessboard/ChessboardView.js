b331da1f2f38d89d7fb2b5d79d9e4bfccb0c0f00
unknown <ronicolajr@yahoo.com>
2019-05-03 21:57:48 -0400

    Adjusted audio code that records and listens.  Added some comments

--- wwwroot/js/Views/Chessboard/ChessboardView.js
+++ wwwroot/js/Views/Chessboard/ChessboardView.js
@@ -1,66 +1,3 @@
-var constraints = {audio: true, video: false};
-var stat = 0;
-var mRecorder;
-function audioConnDisconn(ev) {
-    if(stat == 0) {
-        console.log("Status is disconnected. Now connecting.");
-        stat = 1;
-        document.getElementById('phone').src = '/images/connected.png';
-        //var remoteMedia = document.getElementById('remote');
-
-       // navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream){
-         //   const userAudio = document.querySelector('audio');
-           // userAudio.srcObject = mediaStream;
-            //userAudio.onloadedmetadata = function(ev){
-              //  userAudio.play();
-           // }
-           // console.log("it got here");
-        //});
-      navigator.getUserMedia(constraints, mediaSuccess, medError);
-    } else {
-        console.log("Status is connected.  Now disconnecting");
-        stat = 0;
-        document.getElementById('phone').src = "/images/disconnected.png";
-        mRecorder.stop();
-    }
-}
-
-function mediaSuccess(stream){
-    mRecorder = new MediaStreamRecorder(stream);
-    mRecorder.mimeType = 'audio/wav';
-    mRecorder.ondataavailable = function (blob) {
-        var blobURL = URL.createObjectURL(blob);
-        console.log('<a href="' + blobURL + '">', '</a>');
-    };
-    mRecorder.start(2000);
-}
-
-function medError(e){
-    console.error('media error', e);
-}
-    //audio.srcObject = mediaStream;
-    //audio.onloadedmetadata = function(e){
-      //  audio.play();
-
-  //  };
-//} else{
-  //  audio.onloadedmetadata = function(e){
-    //    audio.pause();
-    //}
-
-//}).catch(function(err){console.log(err.name + err.message);});//};
-
-//record
-//function recordAudio(stream){
- //   var audioRecorder = new MediaStreamRecorder(stream);
-   // audioRecorder.mimeType = 'Audio';
-    //audioRecorder.ondataavailable = function (blob){
-      //  var blobUrl = URL.createObjectURL(blob);
-        //document.write('<a href="' + blobURL + '">' + blobUrl + '</a>');
-    //};
-    //audioRecorder.start(3000);
-
-//}
 
         var moveNumber = 0;
         var count = 0;
@@ -156,4 +93,42 @@ function medError(e){
             }
             saveBoard();
         }
- 
+        //Code for audio chat and recording.  Needs deployed to a server before testing and implementing peer to peer connections for the audio chat and to verify saving.
+        var constraints = {audio: true, video: false};
+        var stat = 0;
+        var mRecorder;
+        function audioConnDisconn(ev) {
+            if(stat == 0) {
+                console.log("Status is disconnected. Now connecting.");
+                stat = 1;
+                document.getElementById('phone').src = '/images/connected.png';
+                navigator.getUserMedia(constraints, mediaSuccess, medError);
+                //For audio playback, commented out becuase peer to peer connections are needed which requires server deployment in order to test and handle the connection and audio stream between multiple users.
+                //navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream){
+                    //const userAudio = document.querySelector('audio');
+                    //userAudio.srcObject = mediaStream;
+                    //userAudio.onloadedmetadata = function(ev){
+                        //userAudio.play();
+                    //}
+                    //console.log("it got here");
+                //});
+            } else {
+                console.log("Status is connected.  Now disconnecting");
+                stat = 0;
+                document.getElementById('phone').src = "/images/disconnected.png";
+                mRecorder.stop();
+                //userAudio.stop();
+            }
+        }
+        function mediaSuccess(stream){
+            mRecorder = new MediaStreamRecorder(stream);
+            mRecorder.mimeType = 'audio/wav';
+            mRecorder.ondataavailable = function (blob) {
+                var blobURL = URL.createObjectURL(blob);
+                console.log(blobURL);
+            };
+            mRecorder.start(2000);
+        }
+        function medError(e){
+            console.error('media error', e);
+        } 
